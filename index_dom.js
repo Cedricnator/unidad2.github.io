8ea9872790615aeb55ae78900b0de4d85fe5e6f9
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-24 19:21:04 -0400

    Add animation for character and nemesis attack in evento.css and index_dom.js

--- index_dom.js
+++ index_dom.js
@@ -10,17 +10,21 @@ document.addEventListener("keydown", (e) => {
     moveBall2(e,".ball2",".stage");
     const isEnableToAtack = attackIfClose(".ball", ".ball2");
     if(isEnableToAtack){
-        document.getElementById("ball").style.backgroundColor = "red";
+        document.getElementById("ball").style.backgroundColor  = "red";
         document.getElementById("ball2").style.backgroundColor = "red";
         
         document.addEventListener("keydown", function (event) {
             if (event.key === "x") {
               character.atack(nemesis, 30)
+              document.getElementById("nemesisimg").classList.add("atacked");
               document.getElementById("enemyHealth").value = nemesis.getHealth();
-        
+              setTimeout(() => document.getElementById("nemesisimg").classList.remove("atacked"), 500);
+
             } else if (event.key === "n") {
               nemesis.atack(character, 5)
+              document.getElementById("characterimg").classList.add("atacked");              
               document.getElementById("characterHealth").value = character.getHealth();
+              setTimeout(() => document.getElementById("characterimg").classList.remove("atacked"), 500);
             }
         });
     } else {
