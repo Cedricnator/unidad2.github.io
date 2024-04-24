5aea3838eb335930e0beeede89984ed6dfa574d2
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-24 18:44:11 -0400

    Add moveBall2 function to teclado.js and update index_dom.js to use moveBall2 for nemesis movement

--- index_dom.js
+++ index_dom.js
@@ -1,36 +1,16 @@
-import { moveBall, shortcuts } from './teclado.js'
+import { moveBall, shortcuts, moveBall2 } from './teclado.js'
 import { Fighter } from './js/fighter.js';
 
-const keysPressed = {};
-const eventsCharacterKey = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
-const eventsNemesisKey   = ["a", "d", "w", "s"];
+
 const character = new Fighter("fighter", 100, 0, 0, 10);
 const nemesis   = new Fighter("nemesis", 100, 0, 0, 10);
 
 document.addEventListener("keydown", (e) => {
     shortcuts(e);
-    keysPressed[e.key] = true;
+    moveBall(e, ".ball", ".stage");
+    moveBall2(e, ".ball2", ".stage");
 });
 
-document.addEventListener("keyup", (e) => {
-    keysPressed[e.key] = false;
-});
-
-const move = () => {
-    eventsCharacterKey.forEach((key) => {
-        if (keysPressed[key]) {
-            moveBall({ key }, ".ball", ".stage", eventsCharacterKey);
-        }
-    });
-    eventsNemesisKey.forEach((key) => {
-        if (keysPressed[key]) {
-            moveBall({ key }, ".ball2", ".stage", eventsNemesisKey);
-        }
-    });
-    requestAnimationFrame(move);
-};
-
-move();
 
 
 document.addEventListener("keydown", function (event) {
--- teclado.js
+++ teclado.js
@@ -1,32 +1,33 @@
 const d = document;
 let x = 0, y = 0;
+let x2 = 0, y2 = 0;
 
-export function moveBall(e, ball, stage, eventsKey){
+export function moveBall(e, ball, stage){
 
     const $ball      = d.querySelector(ball)
     const $stage     = d.querySelector(stage)
     const limitBall  = $ball.getBoundingClientRect();
     const limitStage = $stage.getBoundingClientRect(); 
- 
-    const index = eventsKey.indexOf(e.key);
+    console.log(e)
 
-    switch( index ){
-        case 0:
+    switch( e.keyCode ){
+        case 65:
             if( limitBall.left > limitStage.left){
+                e.preventDefault();
                 x--
             };
             break;
-        case 1:
+        case 68:
             if( limitBall.right < limitStage.right){
                 x++
             };
             break;
-        case 2:
+        case 87:
             if(limitBall.top > limitStage.top){
                 y--
             };
             break;
-        case 3:
+        case 83:
             if(limitBall.bottom < limitStage.bottom){
                 y++
             };
@@ -34,11 +35,51 @@ export function moveBall(e, ball, stage, eventsKey){
         default:
             break;
     }
-    move($ball);
+    move($ball, x, y);
+}   
+
+const move = (ball, x, y) => {
+    ball.style.transform = `translate(${ x * 10 }px, ${ y * 10 }px)`;
 }
 
-const move = ($ball) => {
-    $ball.style.transform = `translate(${ x * 10 }px, ${ y * 10 }px)`;
+export const moveBall2 = (e, ball2, stage) => {
+    const $ball2      = d.querySelector(ball2)
+    const $stage     = d.querySelector(stage)
+    const limitBall  = $ball2.getBoundingClientRect();
+    const limitStage = $stage.getBoundingClientRect(); 
+
+    switch(e.key){
+        case "ArrowLeft":
+            if( limitBall.left > limitStage.left){
+                e.preventDefault();
+                x2--
+            };
+        break;
+
+        case "ArrowRight":
+            if( limitBall.right < limitStage.right){
+                e.preventDefault();
+                x2++
+            };
+        break;
+    
+        case "ArrowUp":
+            if( limitBall.top > limitStage.top){
+                e.preventDefault();
+                y2--
+            };
+        break;
+
+        case "ArrowDown":
+            if( limitBall.bottom < limitStage.bottom){
+                e.preventDefault();
+                y2++
+            };
+        break;
+        default:
+            break;
+    }
+    move($ball2, x2, y2);
 }
 
 
